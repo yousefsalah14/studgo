9bc86dfd0da4eb0904e82d880037fee02e6197df
Yousef Salah <yousefsalah1242003@gmail.com>
2025-04-01 22:10:27 +0200

    fix calendar bug

--- src/components/SA/components/EventCalendar.jsx
+++ src/components/SA/components/EventCalendar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"; // Default styles
-import "./../style/Calendar.css"; // Custom styles
+import "../style/Calendar.css"; // Custom styles
 
 const events = [
   {

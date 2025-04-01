a1c0df231883a7ced576638dece512e597cec177
Yousef Salah <yousefsalah1242003@gmail.com>
2025-04-01 22:22:14 +0200

    .

--- src/components/SA/components/EventCalendar.jsx
+++ src/components/SA/components/EventCalendar.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css"; // Default styles
-import "../../../styles/calendar.css"; // Custom styles
+// import "../../../styles/calendar.css"; // Custom styles
 
 const events = [
   {
